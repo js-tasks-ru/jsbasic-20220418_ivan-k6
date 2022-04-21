7677ecb17d7236cc2ae434b042fdb236f022477e
Ivan Kozlovskiy <KozlovskiiIV@yandex.ru>
2022-04-21 23:28:51 +0300

    Решение задач 1.1 - 1.5

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,12 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str === null || str === undefined || maxlength === null || Number.isNaN(+maxlength)) {
+    console.log('ошибка ввода');
+    return null;
+  }
+  
+  if (str.length <= maxlength) {
+    return str;
+  } else {
+    return str.slice(0, maxlength - 1) + '…';
+  }
 }
